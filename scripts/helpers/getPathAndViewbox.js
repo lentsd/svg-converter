c3e990ba23fcda292c17af3e6298678ff26f32bc
dnotrad <lentsd@bk.ru>
2021-11-30 19:05:45 +0300

    create build

--- scripts/helpers/getPathAndViewbox.js
+++
@@ -1,8 +0,0 @@
-export const  getPathAndViewbox = (svg) => {
-    const path = svg.match(/(?<=d=")[^"]*/)[0]
-    const width = svg.match(/(?<=width=")\d+/)[0];
-    const height = svg.match(/(?<=height=")\d+/)[0];
-  
-    return {width, height, path};
-  }
-  
--- scripts/helpers/round.js
+++
@@ -1,7 +0,0 @@
-export const round = (value) => {
-    return value
-      .toFixed(3)
-      .replace(/0+$/, '')
-      .replace(/\.$/, '')
-  }
-  
